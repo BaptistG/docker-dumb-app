98d12604cc0a8735eea94e16218a2bc1b061594f
Baptiste Guerin <baptisteg@padok.fr>
2020-04-15 12:24:46 +0200

    fixed issues with updating the app

--- aws_shell/setup.sh
+++ aws_shell/setup.sh
@@ -27,9 +27,12 @@ if [ -d docker-dumb-app ]; then rm -rf docker-dumb-app; fi
 git clone -b $1 https://github.com/BaptistG/docker-dumb-app.git
 cd docker-dumb-app
 
+# Stop all running docker containers
+sudo docker ps -q --filter "name=dumb_app" | grep -q . && sudo docker stop dumb_app && sudo docker rm -fv dumb_app
+
 # Build image from dockerfile
 sudo docker build -t dumb_app .
 
 # Launch container and map port 3000 on host machine to port 8080 in container)
 # Running a detached container so that everything works when the user disconnects from the server
-sudo docker run -p 3000:8080 -d dumb_app
+sudo docker run -d -p 3000:8080 --name=dumb_app dumb_app
