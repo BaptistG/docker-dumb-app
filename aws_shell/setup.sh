63ae064e047c52f0609259da216a513e21ec37ea
Baptiste Guerin <baptisteg@padok.fr>
2020-04-15 15:27:25 +0200

    removing unused images

--- aws_shell/setup.sh
+++ aws_shell/setup.sh
@@ -30,7 +30,12 @@ cd docker-dumb-app
 # Stop all running docker containers
 sudo docker ps -q --filter "name=dumb_app" | grep -q . && sudo docker stop dumb_app && sudo docker rm -fv dumb_app
 
-# Build image from dockerfile
+
+# Remove all images (we don't need old images and they'll fill the disk space until the disk is full)
+sudo docker image prune -f
+
+# Build image from dockerfile, you should usually tag the image because if you dont it'll automatically use latest and when you build again it'll overwrite that tag
+# This means you won't be able to use the previous image
 sudo docker build -t dumb_app .
 
 # Launch container and map port 3000 on host machine to port 8080 in container)
